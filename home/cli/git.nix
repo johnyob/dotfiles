3bf33315ac72cabbfa6cfaf90b55e8aad8084638
johnyob <ajo41@cam.ac.uk>
2024-08-06 15:18:43 +0100

    feat(git): add command aliases

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -4,6 +4,23 @@
     userName = "johnyob";
     userEmail = "ajo41@cam.ac.uk";
 
+    aliases = {
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+
+      # Get the current branch name (not so useful in itself, but used in other aliases)
+      branch-name = "!git rev-parse --abbrev-ref HEAD";
+
+      # Push the current branch to the remote "origin", and set it to track
+      # the upstream branch
+      publish = "!git push -u origin $(git branch-name)";
+
+      # Delete the remote version of the current branch
+      unpublish = "!git push origin :$(git branch-name)";
+
+      # Checkout branches via fzf
+      cof = "!git checkout $(git branch --color=always --all --sort=-committerdate | grep -v HEAD | fzf --height 50% --ansi --no-multi --preview-window right:65%  --preview 'git log -n 50 --color=always --date=short --pretty=\"format:%C(auto)%cd %h%d %s\" $(sed \"s/.* //\" <<< {})' | sed \"s/.* //\")";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
 
--- home/cli/zsh.nix
+++ home/cli/zsh.nix
@@ -17,8 +17,33 @@
 
       # opam configuration
       [[ ! -r /Users/ajob410/.opam/opam-init/init.zsh ]] || source /Users/ajob410/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
-
-      eval "$(direnv hook zsh)"
     '';
+
+    shellAliases = {
+      # Git aliases
+      g = "git";
+      gl = "git lol";
+      gs = "git status";
+      ga = "git add";
+
+      gc = "git commit";
+      gca = "git commit --amend";
+      gcr = "git commit --amend --no-edit";
+
+      gco = "git checkout";
+      gcof = "git cof";
+
+      gf = "git fetch";
+      gfa = "git fetch --all";
+      gfo = "git fetch origin";
+
+      gp = "git push";
+      gpf = "git push -f";
+      gpb = "git publish";
+
+      gpick = "git cherry-pick";
+
+      reload = "exec $SHELL -l";
+    };
   };
 }
